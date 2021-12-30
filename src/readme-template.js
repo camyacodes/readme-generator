df7c07ddaf870fb34279e430ae8476a9c088c68f
camyacodes <camyacodes@gmail.com>
2021-12-30 17:47:38 -0500

    full readme template

--- src/readme-template.js
+++ src/readme-template.js
@@ -1,11 +1,48 @@
-const generateReadme = (projectTitle, githubName, email) => {
+const generateReadme = (projectTitle, description, installation, usage, license, contributing, tests, githubName, email) => {
 	return `
     # ${projectTitle}
+ 
+    ## Description
+
+    ${description}
+    
+    ## Table of Contents (Optional)
+    
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [Credits](#credits)
+    * [License](#license)
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [Questiona](#questions)
+    
+    ## Installation
+
+    ${installation}
+    
+    ## Usage 
+
+    ${usage}
+    
+    ## License
+    
+    ${license}
+
+    ## Contributing
+    
+    ${contributing}
+
+    ## Tests
+    
+    ${tests}
 
     ## Questions
-    Checkout my GitHub: ${githubName}
-    You can reach me at ${email}
+
+    GitHub profile: https://github.com/${githubName}
+
+    Contact email: ${email}
 `;
 };
 
 module.exports = generateReadme;
+
