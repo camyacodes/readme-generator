5608bf7477801978954c97bb0bfaa1c35b8e8a62
camyacodes <camyacodes@gmail.com>
2022-01-01 11:11:47 -0500

    read me template updated

--- src/readme-template.js
+++ src/readme-template.js
@@ -1,48 +1,34 @@
-const generateReadme = (projectTitle, description, installation, usage, license, contributing, tests, githubName, email) => {
-	return `
-    # ${projectTitle}
- 
-    ## Description
-
-    ${description}
-    
-    ## Table of Contents (Optional)
-    
-    * [Installation](#installation)
-    * [Usage](#usage)
-    * [Credits](#credits)
-    * [License](#license)
-    * [Contributing](#contributing)
-    * [Tests](#tests)
-    * [Questiona](#questions)
-    
-    ## Installation
-
-    ${installation}
-    
-    ## Usage 
-
-    ${usage}
-    
-    ## License
-    
-    ${license}
-
-    ## Contributing
-    
-    ${contributing}
-
-    ## Tests
-    
-    ${tests}
-
-    ## Questions
-
-    GitHub profile: https://github.com/${githubName}
-
-    Contact email: ${email}
-`;
-};
-
-module.exports = generateReadme;
-
+// generateReadme function populating the README.md
+function generateReadme(answers) {
+    return `
+<h1 align="center">${answers.projectTitle}</h1>
+  
+![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)<br />
+## Description
+${answers.description}
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+## Installation
+${answers.installation}
+## Usage
+${answers.usage}
+## License
+This application is covered by the ${answers.license} license. 
+## Contributing
+${answers.contributing}
+## Tests
+${answers.tests}
+## Questions
+Email me with any questions: ${answers.email}<br /><br />
+Find me on GitHub: (https://github.com/${answers.username})<br />
+<br />
+    `;
+  }
+  
+  module.exports = generateReadme;
