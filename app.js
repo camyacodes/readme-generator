1926d20f3dee68f234567e0bba07e7ddca3b5a4a
camyacodes <camyacodes@gmail.com>
2021-12-31 23:11:42 -0500

    inquirer install

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
-const generateReadme = require('./src/readme-template.js')
-
-const fs = require("fs");
-
-const readmeDataArgs = process.argv.slice(2, process.argv.length);
-
-const [projectTitle, githubName, email] = readmeDataArgs;
-
-// const readmeData = readmeDataArr => {
-//     readmeDataArr.forEach(readmeItem => console.log(readmeItem));
-//   };
-
-// printReadmeData(readmeDataArgs);
+const inquirer = require('inquirer');
+// const fs = require("fs");
+// const generateReadme = require('./src/readme-template.js')
 
 
+// const pageREADME = generateReadme(projectTitle, description, installation, usage, license, contributing, tests, githubName, email);
 
-fs.writeFile(
-	"README.md",
-	generateReadme(projectTitle, githubName, email),
-	(err) => {
-		if (err) throw err;
 
-		console.log(
-			"Portfolio complete! Check out testREADME.html to see the output!"
-		);
-	}
-);
+// fs.writeFile(
+// 	"README.md",
+// 	pageREADME,
+// 	(err) => {
+// 		if (err) throw err;
+
+// 		console.log(
+// 			"Portfolio complete! Check out testREADME.html to see the output!"
+// 		);
+// 	}
+// );
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
